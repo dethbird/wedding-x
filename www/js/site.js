18fb5dd9121bc03fd03a775a8f49e1195022a5fb
Rishi Satsangi <rishi.satsangi@gmail.com>
2015-03-22 00:35:32 -0400

    scurrying mouse

--- www/js/site.js
+++ www/js/site.js
@@ -9,6 +9,26 @@ var scale = 1.0;
 
 $(window).load(function(){
 
+	/**
+ 	 * @var $w the window
+ 	 */
+	var w = $(window);
+
+	/**
+ 	 * @var $o the containing div
+ 	 */
+	var o = $('.outer');
+	
+	/**
+ 	 * @var $scale global scale modifier
+ 	 */
+	var scale = 1.0;
+
+	/**
+ 	 * @var $timelines array of animation timelines
+ 	 */
+	var timelines = [];
+
 	// functions
 	var rescale = function(){
 		scale = w.outerWidth() / configs.container.width;
@@ -16,46 +36,61 @@ $(window).load(function(){
 		$('.object').each(function(i,object){
 			var object = $(object);
 			object.css({
-				height: Math.floor(scale * object.data("height")) + 'px',
-				width: Math.floor(scale * object.data("width")) + 'px',
-				top: Math.floor(scale * object.data("top")) + 'px',
-				left: Math.floor(scale * object.data("left")) + 'px'
+				height: Math.floor(scale * object.data('height')) + 'px',
+				width: Math.floor(scale * object.data('width')) + 'px',
+				top: Math.floor(scale * object.data('top')) + 'px',
+				left: Math.floor(scale * object.data('left')) + 'px'
 			})
 		});
 	}
 
-	// vars
-	var w = $(window);
-	var o = $('.outer');
-	var scale = 1.0;
+	var scurry = function(object) {
+		object.css({
+			height: Math.floor(scale * object.data('height') * object.attr('data-scurry-start-scale')),
+			width: Math.floor(scale * object.data('width') * object.attr('data-scurry-start-scale'))
+		});
+
+		TweenLite.to(
+			object,
+			object.attr('data-scurry-duration'),
+			{
+				left: w.width() + object.width(),
+				top: Math.floor(parseInt(object.attr('data-scurry-end-top')) * scale),
+				height: Math.floor(scale * object.data('height') * object.attr('data-scurry-end-scale')),
+				width: Math.floor(scale * object.data('width') * object.attr('data-scurry-end-scale')),
+				ease: Power1.easeIn,
+				onComplete: function(){
+					console.log(scale);
+					object.css({left: object.data('left')});
+					object.css({top: object.data('top')});
+					var min = 1000 * parseInt(object.attr("data-scurry-random-delay-min"));
+					var max = 1000 * parseInt(object.attr("data-scurry-random-delay-max"));
+					var timeout = Math.floor((Math.random() * (max - min)) + min);
+					var t = setTimeout(scurry, timeout, object);
+				}
+			}
+		);
+	}
+
 
 	// set initial outer box width
 	o.css({
-		width:configs.container.width + "px",
-		height:configs.container.height + "px"
+		width:configs.container.width + 'px',
+		height:configs.container.height + 'px'
 	});
 
-	// array of animation timelines
-	var timelines = [];
 
 	// set intial data for all objects
 	$('.object').each(function(i,object){
 		var object = $(object);
-		// save dimensions
-		object.data("height", object.height());
-		object.data("width", object.width());
-		// save position
-		var pos = object.position();
-		object.data("top", pos.top);
-		object.data("left", pos.left);
 
 		// set the original src
 		if (object.prop('tagName')=='IMG') {
-			object.data("src", object.attr("src"));
+			object.data('src', object.attr('src'));
 			// set the swappable src
 			if(object.hasClass('swap')) {
 				var objectSwap = $('#' + object.attr('id') + '-swap');
-				object.data("src-swap", objectSwap.attr('src'));
+				object.data('src-swap', objectSwap.attr('src'));
 
 				object.mouseover(function(){
 					object.attr('src', object.data('src-swap'));
@@ -71,6 +106,24 @@ $(window).load(function(){
 			repeatTimeline.add(TweenMax.to(object, object.attr('data-rotate-duration'), {rotationZ: 360, ease: Power0.easeNone}));
 			timelines.push(repeatTimeline);
 		}
+
+		if (object.hasClass('scurry')) {
+			object.css({
+				left: '-' + (object.width() + 10) + 'px'
+			});
+
+			var t = setTimeout(scurry, parseInt(object.attr('data-scurry-initial-delay')) * 1000, object);
+		}
+
+		// save dimensions
+		object.data('height', object.height());
+		object.data('width', object.width());
+		// save position
+		var pos = object.position();
+		object.data('top', pos.top);
+		object.data('left', pos.left);
+
+
 
 	});
 
